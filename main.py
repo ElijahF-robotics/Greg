c2fdb60284e181033605f9cacf040c80fa426fe3
ElijahF-robotics <elijah.flader@g.blsd.us>
2023-11-29 10:04:24 -0500

    First substantial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# Greg is an automated robot. He is an interface to program in various different functions.
+# He is currently a work in progress, and will be till he does everything important
+
+import alpaca_trade_api as tradeapi
