88451cda340e29b0cdd21366cf7ac56dd3be7f7b
ElijahF-robotics <elijah.flader@g.blsd.us>
2023-12-01 10:10:14 -0500

    Unverisoned Files Added

--- stockBot.py
+++ stockBot.py
@@ -1,45 +1,63 @@
 # This is the code for Greg's stock bot functions. This is the code that is used to manage the stock account
 
-import alpaca.trading as tradeAPI
+from alpaca.trading.client import TradingClient
+from alpaca.trading.enums import OrderSide, OrderType, TimeInForce
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.stream import TradingStream
 import numpy as np
 
-# The secret key and public key for my paper stock account
+# The global variables everything else needs
 SEC_KEY = 'erU539BdUUsStoJCLuJANSgI6cI8TEYj9gcZbpRd'  # Enter Your Secret Key Here
 PUB_KEY = 'PKZBJS2I4QCXLGEI9V5T'  # Enter Your Public Key Here
 BASE_URL = 'https://paper-api.alpaca.markets'  # This is the base URL for paper trading
-api = tradeAPI.REST(key_id=PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL)  # For real trading, don't enter a base_url
+client = TradingClient(key_id=PUB_KEY, secret_key=SEC_KEY, paper=True)  # This is the client object
+account = dict(client.get_account())  #This is the account object
 
 
-def getAccount():
-    # This function will return the account information
-    return api.get_account()
+def getAccountData():
+    # This funtion will return the account data
+    assets = [i for i in client.get_all_positions]
+    positions = [(asset.symbol, asset.qty, asset.current_price) for asset in assets]
+    return positions
 
 
 def getPosition(ticker):
     # This function will return the position of the stock
     # ticker is the ticker of the stock
-    try:
-        return api.get_position(ticker)
-    except:  # If the stock is not in the portfolio, return 0
-        return 0
+    positions = getAccountData()
+    for position in positions:
+        if position[0] == ticker:
+            return position[1]
+
+    return 0
 
 
 def manageStock(ticker, order):
     # This function will manage the stock account
     # ticker is the ticker of the stock
-    # order is the order to be placed
 
-    if order == "buy" or order == "sell":
-        api.submit_order(
+    if order == "hold":
+        return
+    elif order == "buy":
+        orderDetails = MarketOrderRequest(
             symbol=ticker,
             qty=1,
-            side=order,
-            type='market',
-            time_in_force='gtc')
+            side=OrderSide.BUY,
+            type=OrderType.MARKET,
+        )
+    elif order == "sell":
+        orderDetails = MarketOrderRequest(
+            symbol=ticker,
+            qty=1,
+            side=OrderSide.SELL,
+            type=OrderType.MARKET,
+        )
 
-    else:
-        return 0
+    client.submit_order(order_data=orderDetails)
 
+
+def closeEverything():
+    client.close_all_positions()
 
 def getMarketData(ticker):
     # This function will return the market data of the stock
